0ad7f4c5a2db089784e940412fb748544faff059
tlkahn <joshgy@gmail.com>
2017-09-04 12:59:34 +0800

    Initial Commit

---
+++ Yan/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Yan
+//
+//  Created by toeinriver on 9/4/17.
+//  Copyright © 2017 toeinriver. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let synthesizer = AVSpeechSynthesizer()
+        let utterance = AVSpeechUtterance(string: "郭永是世界上最棒的！")
+        utterance.voice = AVSpeechSynthesisVoice.init(language: "zh-CN")
+        synthesizer.speak(utterance)
+        
+        let soundPath = Bundle.main.url(forResource: "piano", withExtension: "mp3")!
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        
+            player = try AVAudioPlayer(contentsOf: soundPath)
+            guard let player = player else { return }
+        
+            player.play()
+        } catch let error as NSError {
+            print(error.description)
+        }
+        // print(FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ YanShare/ShareViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ShareViewController.swift
+//  YanShare
+//
+//  Created by toeinriver on 9/4/17.
+//  Copyright © 2017 toeinriver. All rights reserved.
+//
+
+import UIKit
+import Social
+import MobileCoreServices
+import Alamofire
+import SwiftyJSON
+
+class ShareViewController: SLComposeServiceViewController {
+    
+    override func isContentValid() -> Bool {
+        // Do validation of contentText and/or NSExtensionContext attachments here
+        return true
+    }
+    
+    override func didSelectPost() {
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+        let extensionItem = extensionContext?.inputItems.first as! NSExtensionItem
+        let itemProvider = extensionItem.attachments?.first as! NSItemProvider
+        let propertyList = String(kUTTypePropertyList)
+        if itemProvider.hasItemConformingToTypeIdentifier(propertyList) {
+            itemProvider.loadItem(forTypeIdentifier: propertyList, options: nil, completionHandler: { (item, error) -> Void in
+                guard let dictionary = item as? NSDictionary else { return }
+                DispatchQueue.main.async {
+                    if let jsResults = dictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary
+                    {
+                        let jsURL = jsResults["URL"] as? String
+                        print("jsURL: ", jsURL!)
+                        let parameters: Parameters = ["token": "6b8979cf259d058f1423274e146c06e2", "url": jsURL!]
+                        print("parameters", parameters)
+                        print("sending reqeust to diffbot API")
+                        Alamofire.request("https://api.diffbot.com/v3/article", parameters: parameters)
+                            .responseJSON(completionHandler: { (response: DataResponse) -> Void in
+                                if let data = response.data {
+                                    let json = JSON(data: data)
+                                    print(json["objects"][0]["text"])
+                                    let p: Parameters = ["content": json["objects"][0]["text"]]
+                                    Alamofire.request("http://localhost:3000/articles", method: .post, parameters: p).responseJSON(completionHandler: {
+                                        (response: DataResponse) -> Void in
+                                        print(response)
+                                    })
+                                }
+                            })
+                    }
+                }
+                self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+            })
+        } else {
+            print("error")
+        }
+        
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    }
+    
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+    
+}
